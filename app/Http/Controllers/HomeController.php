aaaef1ef5b9bdde833207d2271f2cef996b9d7bb
rabijati <jattirabi07@gmail.com>
2024-07-03 08:29:56 +0545

    User can see appointments and cancel them

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -62,4 +62,28 @@ class HomeController extends Controller
         return redirect()->back()->with('message', 'Appointment Request Successful. We will contact with you soon');
 
     }
+
+    public function myappointment()
+    {
+        if(Auth::id())
+        {
+            $userid=Auth::user()->id;
+
+            $appoint=appointment::where('user_id',$userid)->get();
+            return view('user.my_appointment',compact('appoint'));
+        }else{
+            return redirect()->back();
+        }
+        
+    }
+
+    public function cancel_appoint($id){
+
+        $data=appointment::find($id);
+
+        $data->delete();
+
+        return redirect()->back();
+
+    }
 }
